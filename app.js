4d8c35cd12bb46a3c89e68a58993f5e4e6b14b40
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-04-04 21:21:48 +0200

    added a global middleware error handling.

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/subscriptions', subscriptionRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
+
+app.use(errorMiddleware);
 
 
 app.get('/', (req, res) => {
---
+++ middlewares/error.middleware.js
@@ -0,0 +1,36 @@
+ const errorMiddleware = (err, req, res, next) => {
+    try {
+        let error = [ ...err ];
+
+        error.message = err.message 
+
+        console.error(err);
+
+        //mongoose bad object id error
+        if (err.name === 'CastError') {
+            const message = `Resource not found.`;
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+
+        //mongoose duplicate key error
+        if (err.code === 11000) {
+            const message = `Duplicate field value entered.`;
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        //mongoose validation error
+        if (err.name === 'ValidationError') {
+            const message = Object.values(err.errors).map(value => value.message);
+            error = new Error(message.join(','));
+            error.statusCode = 400;
+        }
+        res.status(error.statusCode || 500).json({ success: false, message: error.message || 'Internal Server Error' });
+
+    } catch (error) {
+        next(error);
+    }
+ }
+
+ export default errorMiddleware;
